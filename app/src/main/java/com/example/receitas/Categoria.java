83bad3783c7b50a20dd31375f394611e89c69d00
diogosouzas <diogosouza@rede.ulbra.br>
2019-05-03 01:40:28 -0300

    Implementado Serializable

--- app/src/main/java/com/example/receitas/Categoria.java
+++ app/src/main/java/com/example/receitas/Categoria.java
@@ -1,6 +1,8 @@
 package com.example.receitas;
 
-public class Categoria {
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
 
     private Integer id;
     private String nome;
